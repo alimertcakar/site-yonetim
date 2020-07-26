857335e3da2588227562a23b9aa900b89e155475
Ali Mert <mail@alimert.net>
2020-07-26 23:19:51 +0300

    removed steps

--- pages/addcommunity.tsx
+++ pages/addcommunity.tsx
@@ -2,188 +2,22 @@ import React from "react";
 import Container from "@material-ui/core/Container";
 import { makeStyles } from "@material-ui/styles";
 import TextField from "@material-ui/core/TextField";
-import Stepper from "@material-ui/core/Stepper";
-import Step from "@material-ui/core/Step";
 import Grid from "@material-ui/core/Grid";
-import Paper from "@material-ui/core/Paper";
 
 import StepButton from "@material-ui/core/StepButton";
 import Link from "../src/Link";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 
-import StepOne from "src/Components/AddCommunity/StepOne";
-import StepTwo from "src/Components/AddCommunity/StepTwo";
-import StepThree from "src/Components/AddCommunity/StepThree";
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    width: "100%",
-  },
-  button: {
-    marginRight: theme.spacing(1),
-  },
-  backButton: {
-    marginRight: theme.spacing(1),
-  },
-  completed: {
-    display: "inline-block",
-  },
-  instructions: {
-    marginTop: theme.spacing(1),
-    marginBottom: theme.spacing(1),
-  },
-}));
-
-function getSteps() {
-  return [
-    "Sitenizin ismi nedir?",
-    "Sitenizde kaç apartman var?",
-    "Kaç daire var ve kimler oturuyor?",
-  ];
-}
-
-function getStepContent(step) {
-  switch (step) {
-    case 0:
-      return <StepOne />;
-    case 1:
-      return <StepTwo />;
-    case 2:
-      return <StepThree />;
-    default:
-      return "Hata";
-  }
-}
+const useStyles = makeStyles((theme) => ({}));
 
 export default function HorizontalNonLinearAlternativeLabelStepper() {
   const classes = useStyles();
-  const [activeStep, setActiveStep] = React.useState(0);
-  const [completed, setCompleted] = React.useState(new Set());
-  const [skipped, setSkipped] = React.useState(new Set());
-  const steps = getSteps();
-
-  const totalSteps = () => {
-    return getSteps().length;
-  };
-
-  const handleSkip = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep + 1);
-    setSkipped((prevSkipped) => {
-      const newSkipped = new Set(prevSkipped.values());
-      newSkipped.add(activeStep);
-      return newSkipped;
-    });
-  };
-
-  const skippedSteps = () => {
-    return skipped.size;
-  };
-
-  const completedSteps = () => {
-    return completed.size;
-  };
-
-  const allStepsCompleted = () => {
-    return completedSteps() === totalSteps() - skippedSteps();
-  };
-
-  const isLastStep = () => {
-    return activeStep === totalSteps() - 1;
-  };
-
-  const handleNext = () => {
-    const newActiveStep =
-      isLastStep() && !allStepsCompleted()
-        ? // It's the last step, but not all steps have been completed
-          // find the first step that has been completed
-          steps.findIndex((step, i) => !completed.has(i))
-        : activeStep + 1;
-
-    setActiveStep(newActiveStep);
-  };
-
-  const handleBack = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep - 1);
-  };
-
-  const handleStep = (step) => () => {
-    setActiveStep(step);
-  };
-
-  const handleComplete = () => {
-    const newCompleted = new Set(completed);
-    newCompleted.add(activeStep);
-    setCompleted(newCompleted);
-
-    /**
-     * Sigh... it would be much nicer to replace the following if conditional with
-     * `if (!this.allStepsComplete())` however state is not set when we do this,
-     * thus we have to resort to not being very DRY.
-     */
-    if (completed.size !== totalSteps() - skippedSteps()) {
-      handleNext();
-    }
-  };
-
-  function isStepComplete(step) {
-    return completed.has(step);
-  }
 
   return (
-    <Container maxWidth="md">
-      <div className={classes.root}>
-        <Stepper alternativeLabel nonLinear activeStep={activeStep}>
-          {steps.map((label, index) => {
-            const stepProps = {};
-            const buttonProps = {};
-
-            return (
-              <Step key={label} {...stepProps}>
-                <StepButton
-                  onClick={handleStep(index)}
-                  completed={isStepComplete(index)}
-                  {...buttonProps}
-                >
-                  {label}
-                </StepButton>
-              </Step>
-            );
-          })}
-        </Stepper>
-        <div>
-          {allStepsCompleted() ? (
-            <div>
-              <Typography className={classes.instructions}>
-                Tamamlandı
-              </Typography>
-            </div>
-          ) : (
-            <div>
-              <Typography className={classes.instructions}>
-                {getStepContent(activeStep)}
-              </Typography>
-              <div>
-                <Button
-                  disabled={activeStep === 0}
-                  onClick={handleBack}
-                  className={classes.button}
-                >
-                  Geri
-                </Button>
-
-                <Button
-                  variant="contained"
-                  color="primary"
-                  onClick={handleComplete}
-                >
-                  {completedSteps() === totalSteps() - 1 ? "Kaydet" : "İleri"}
-                </Button>
-              </div>
-            </div>
-          )}
-        </div>
-      </div>
-    </Container>
+    <div>
+      Site ismi belirle
+      <TextField />
+    </div>
   );
 }
